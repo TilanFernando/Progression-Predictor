d6e518d9f2eeb24771ce60cfec1fe27c1812947a
TilanFernando <152051887+TilanFernando@users.noreply.github.com>
2024-08-17 20:35:03 +0530

    Added a function to get the progression outcome

--- University Progression Prediction System.py
+++ University Progression Prediction System.py
@@ -64,4 +64,50 @@ def valid_credits():
                 else:
                     break
 
-        
+        #Defining function to get the progression outcome-----------------------------------------------------------
+
+def progression_outcome():
+
+    #global variables
+    global credit_pass,credit_defer,credit_fail,progress_count,module_trailer_count,module_retriever_count,exclude_count,count
+
+    count += 1 #total inputs count
+
+    # using loop to select progression outcome when the if conditions are satisfied
+    while True:
+
+        if credit_pass ==120 and credit_defer == 0 and credit_fail == 0:
+
+                print("----------------------------------------------------------")
+                print("PROGRESS")
+                print("\n")
+                progress_count += 1 #count for the histogram
+                input_progression_data["Progress"].append(str(credit_pass) + ", " + str(credit_defer) + ", " + str(credit_fail))#according to progression outcome obtaining input progression data 
+                break
+            
+        elif credit_pass == 100 and (credit_defer ==20 or credit_fail == 20):
+                
+                print("----------------------------------------------------------")
+                print("PROGRESS-MODULE TRAILER")
+                print("\n")
+                module_trailer_count += 1
+                input_progression_data["Progress (module trailer)"].append(str(credit_pass) + ", " + str(credit_defer) + ", " + str(credit_fail))#according to progression outcome obtaining input progression data 
+                break
+            
+        elif credit_pass <= 80 and (credit_defer >= 40 or credit_fail < 80) or (credit_defer <= 40 or credit_fail <= 80):
+                
+                print("----------------------------------------------------------")
+                print("DO NOT PROGRESS-MODULE RETRIEVER")
+                print("\n")
+                module_retriever_count += 1
+                input_progression_data["Do not Progress – module retriever"].append(str(credit_pass) + ", " + str(credit_defer) + ", " + str(credit_fail))#according to progression outcome obtaining input progression data 
+                break
+            
+        elif credit_pass in range(0,41,20) and (credit_defer < 40 or credit_fail >= 80):
+                
+                print("----------------------------------------------------------")
+                print("EXCLUDE")
+                print("\n")
+                exclude_count += 1
+                input_progression_data["Exclude"].append(str(credit_pass) + ", " + str(credit_defer) + ", " + str(credit_fail))#according to progression outcome obtaining input progression data 
+                break
